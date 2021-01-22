f9552dcb96b9fa597bc2f5571f7a82db6dc0e086
Hassan <hassanali.naqvi@ajsoftware.com.pk>
2021-01-23 00:06:55 +0500

    updayed

--- src/app/sub-header/sub-header.component.ts
+++ src/app/sub-header/sub-header.component.ts
@@ -7,9 +7,31 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class SubHeaderComponent implements OnInit {
   @Input() indicator;
-  constructor() { }
+  selected: string = 'player';
+  placeHolder: string = 'Player';
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  search(selectedValue: string) {
+    this.selected = selectedValue;
+    this.placeHolderSelection(selectedValue);
+  }
+
+  placeHolderSelection(value) {
+    switch (value) {
+      case 'player':
+        this.placeHolder = 'Player';
+        break;
+      case 'clan':
+        this.placeHolder = 'Name, tag or [tag]';
+        break;
+      case 'warship':
+        this.placeHolder = 'Warship';
+        break;
+    }
+  }
 }
